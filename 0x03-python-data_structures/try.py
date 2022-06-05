0495fa10554f3c03edb7449f56e8b8e1b969e2e0
Noble Mungu <mungunoble@gmail.com>
2022-06-06 00:14:36 +0300

    add list

---
+++ 0x03-python-data_structures/try.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list,idx,element):
+    if my_list < 0 < len(my_list):
+        my_list(idx) = element
+        
+        
+        
+    
+    return(my_list)
+    
+    
